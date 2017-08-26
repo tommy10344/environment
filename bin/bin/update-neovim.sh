253fe41d50f6594da12fec63f68a52fca41e477d
Hiroaki Tomiyoshi <htomiyosi@gmail.com>
2017-08-26 23:08:09 +0900

    Revive neovim building

--- bin/bin/update-neovim.sh
+++ bin/bin/update-neovim.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # cd ${HOME}/src/neovim/
-# cd $(ghq root)/github.com/neovim/neovim
-# 
-# git pull origin master
-# make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
-# make install
+cd $(ghq root)/github.com/neovim/neovim
+
+rm -rf build
+git pull origin master
+make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
+make install
