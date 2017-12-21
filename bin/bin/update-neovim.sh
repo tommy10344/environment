a0da147108e5d7fbe954e446be8d55c780352b65
Hiroaki Tomiyoshi <htomiyosi@gmail.com>
2017-12-22 00:44:29 +0900

    update

--- bin/bin/update-neovim.sh
+++ bin/bin/update-neovim.sh
@@ -6,4 +6,5 @@ cd $(ghq root)/github.com/neovim/neovim
 rm -rf build
 git pull origin master
 make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
+make CMAKE_BUILD_TYPE=RelWithDebInfo
 make install
