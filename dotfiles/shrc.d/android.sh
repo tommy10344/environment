7222796d058453230e24cf992c5032363b790965
Hiroaki Tomiyoshi <htomiyosi@gmail.com>
2018-02-23 15:31:53 +0900

    Modify ANDROID_HOME & Add PATH

--- dotfiles/shrc.d/android.sh
+++ dotfiles/shrc.d/android.sh
@@ -1,5 +1,9 @@
 # Android SDK
-if [ -d "/usr/local/opt/android-sdk" ]; then
-    export ANDROID_HOME=/usr/local/opt/android-sdk
-    # export ANDROID_SDK_TOOLS=${ANDROID_HOME}/tools
+
+SDKPATH=${HOME}/Library/Android/sdk
+
+if [ -d "${SDKPATH}" ]; then
+    export ANDROID_HOME=${SDKPATH}
+    export ANDROID_SDK_TOOLS=${ANDROID_HOME}/tools
+    export PATH=${ANDROID_SDK_TOOLS}/bin:$PATH
 fi
