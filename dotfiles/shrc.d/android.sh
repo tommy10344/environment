d8e980bc04eb466314a5b12e872b68eed82bada3
Hiroaki Tomiyoshi <htomiyosi@gmail.com>
2018-05-08 19:02:33 +0900

    Add PATH to Android SDK platform-tools

--- dotfiles/shrc.d/android.sh
+++ dotfiles/shrc.d/android.sh
@@ -5,5 +5,7 @@ SDKPATH=${HOME}/Library/Android/sdk
 if [ -d "${SDKPATH}" ]; then
     export ANDROID_HOME=${SDKPATH}
     export ANDROID_SDK_TOOLS=${ANDROID_HOME}/tools
+    export ANDROID_SDK_PLATFORM_TOOLS=${ANDROID_HOME}/platform-tools
     export PATH=${ANDROID_SDK_TOOLS}/bin:$PATH
+    export PATH=${ANDROID_SDK_PLATFORM_TOOLS}:$PATH
 fi
