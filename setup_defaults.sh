b88e0eea15c504e62a4078e502fb1371380ff0a6
Hiroaki Tomiyoshi <htomiyosi@gmail.com>
2021-12-27 16:33:37 +0900

    Update

--- setup_defaults.sh
+++ setup_defaults.sh
@@ -39,8 +39,8 @@ defaults write com.apple.dock wvous-tl-corner -int 3
 defaults write com.apple.dock wvous-tl-modifier -int 0
 
 # Top right
-# defaults write com.apple.dock wvous-tr-corner -int 4
-# defaults write com.apple.dock wvous-tr-modifier -int 0
+defaults write com.apple.dock wvous-tr-corner -int 11
+defaults write com.apple.dock wvous-tr-modifier -int 0
 
 # Bottom left
 defaults write com.apple.dock wvous-bl-corner -int 4
