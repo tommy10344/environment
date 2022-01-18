c795a76986c26cc1f3ac655e8964b4227bbb86f4
Hiroaki Tomiyoshi <htomiyosi@gmail.com>
2022-01-18 18:36:18 +0900

    Update

--- dotfiles/vscode/setup.sh
+++ dotfiles/vscode/setup.sh
@@ -4,7 +4,6 @@ code --install-extension vscodevim.vim
 code --install-extension editorconfig.editorconfig
 code --install-extension eamodio.gitlens
 code --install-extension esbenp.prettier-vscode
-code --install-extension coenraads.bracket-pair-colorizer
 code --install-extension yzhang.markdown-all-in-one
 code --install-extension davidanson.vscode-markdownlint
 code --install-extension patricklee.vsnotes
