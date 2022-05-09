25025a039096e571659ca0b5bdf64da73de6afbd
Hiroaki Tomiyoshi <htomiyosi@gmail.com>
2022-05-09 15:46:12 +0900

    Update

--- setup_defaults.sh
+++ setup_defaults.sh
@@ -39,12 +39,12 @@ defaults write com.apple.dock expose-group-apps -bool true
 # 12: Notification Center
 
 # Top left
-defaults write com.apple.dock wvous-tl-corner -int 3
-defaults write com.apple.dock wvous-tl-modifier -int 0
+# defaults write com.apple.dock wvous-tl-corner -int 3
+# defaults write com.apple.dock wvous-tl-modifier -int 0
 
 # Top right
-defaults write com.apple.dock wvous-tr-corner -int 2
-defaults write com.apple.dock wvous-tr-modifier -int 0
+# defaults write com.apple.dock wvous-tr-corner -int 2
+# defaults write com.apple.dock wvous-tr-modifier -int 0
 
 # Bottom left
 defaults write com.apple.dock wvous-bl-corner -int 4
