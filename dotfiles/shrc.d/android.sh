ec295205adbd563ec997c69f0c11e9ef9e2dc840
Hiroaki Tomiyoshi <tomiyoshi@onepointfive.co.jp>
2023-10-23 10:57:12 +0900

    Add Xamarin Android SDK Path if Google Android SDK not found

--- dotfiles/shrc.d/android.sh
+++ dotfiles/shrc.d/android.sh
@@ -1,11 +1,14 @@
 # Android SDK
 
-SDKPATH=${HOME}/Library/Android/sdk
+SDKPATH_ANDROID=${HOME}/Library/Android/sdk
+SDKPATH_XAMARIN=${HOME}/Library/Developer/Xamarin/android-sdk-macosx
 
-if [ -d "${SDKPATH}" ]; then
-    export ANDROID_HOME=${SDKPATH}
-    export ANDROID_SDK_TOOLS=${ANDROID_HOME}/tools
-    export ANDROID_SDK_PLATFORM_TOOLS=${ANDROID_HOME}/platform-tools
-    export PATH=${ANDROID_SDK_TOOLS}/bin:$PATH
-    export PATH=${ANDROID_SDK_PLATFORM_TOOLS}:$PATH
+if [ -d "${SDKPATH_ANDROID}" ]; then
+  export ANDROID_HOME=${SDKPATH_ANDROID}
+  export ANDROID_SDK_TOOLS=${ANDROID_HOME}/tools
+  export ANDROID_SDK_PLATFORM_TOOLS=${ANDROID_HOME}/platform-tools
+  export PATH=${ANDROID_SDK_TOOLS}/bin:$PATH
+  export PATH=${ANDROID_SDK_PLATFORM_TOOLS}:$PATH
+elif [ -d "${SDKPATH_XAMARIN}" ]; then
+  export PATH=${SDKPATH_XAMARIN}/platform-tools:$PATH
 fi
