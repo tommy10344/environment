462814067d46882fe3a7ab875aa21947243b8a45
Hiroaki Tomiyoshi <tomiyoshi@onepointfive.co.jp>
2023-10-23 10:05:00 +0900

    Enable VSCode key-repeating

--- dotfiles/vscode/setup.sh
+++ dotfiles/vscode/setup.sh
@@ -37,3 +37,12 @@ code --install-extension christian-kohler.npm-intellisense
 
 # Golang
 code --install-extension golang.go
+
+
+# For VSCodeVim: Enable key-repeating
+# https://github.com/VSCodeVim/Vim
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false              # For VS Code
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false      # For VS Code Insider
+defaults write com.vscodium ApplePressAndHoldEnabled -bool false                      # For VS Codium
+defaults write com.microsoft.VSCodeExploration ApplePressAndHoldEnabled -bool false   # For VS Codium Exploration users
+defaults delete -g ApplePressAndHoldEnabled
