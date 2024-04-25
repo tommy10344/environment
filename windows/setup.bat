78d2a8254ef8621a1dd33e4ea89a4da75964ba48
Hiroaki Tomiyoshi <htomiyosi@gmail.com>
2024-04-25 16:57:31 +0900

    Update and rename setup.bat to setup.ps1

--- windows/setup.bat
+++
@@ -1,7 +0,0 @@
-wsl --install
-
-winget install Microsoft.VisualStudioCode
-winget install Microsoft.VisualStudio.2022.Community
-winget install Atlassian.Sourcetree
-
-winget install Google.Chrome
---
+++ windows/setup.ps1
@@ -0,0 +1,7 @@
+wsl --install
+
+winget install --id Microsoft.VisualStudioCode
+winget install --id Microsoft.VisualStudio.2022.Community
+winget install --id Atlassian.Sourcetree
+winget install --id Google.Chrome
+winget install --id Microsoft.Powershell --source winget
