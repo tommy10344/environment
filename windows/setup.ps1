9ba64cdb3bba3dd67e5e7659038f2d94b27269c1
Hiroaki Tomiyoshi <htomiyosi@gmail.com>
2024-09-30 11:08:38 +0900

    Update setup.ps1

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -5,3 +5,5 @@ winget install --id Microsoft.VisualStudio.2022.Community
 winget install --id Atlassian.Sourcetree
 winget install --id Google.Chrome
 winget install --id Microsoft.Powershell --source winget
+winget install --id Microsoft.PowerToys --source winget
+winget install --id Docker.DockerDesktop --source winget
