c187ffba304f3caf4dcdc9441bf1b162c0a479d5
Hiroaki Tomiyoshi <htomiyosi@gmail.com>
2025-02-18 16:56:31 +0900

    Update PATH for Android

--- dotfiles/shrc.d/android.sh
+++ dotfiles/shrc.d/android.sh
@@ -1,15 +1,14 @@
 # Android SDK
+# https://developer.android.com/tools/variables
 
 SDKPATH_ANDROID=${HOME}/Library/Android/sdk
 SDKPATH_XAMARIN=${HOME}/Library/Developer/Xamarin/android-sdk-macosx
 
 if [ -d "${SDKPATH_ANDROID}" ]; then
   export ANDROID_HOME=${SDKPATH_ANDROID}
-  export ANDROID_SDK_TOOLS=${ANDROID_HOME}/tools
-  export ANDROID_SDK_PLATFORM_TOOLS=${ANDROID_HOME}/platform-tools
-  export PATH=${ANDROID_SDK_TOOLS}/bin:$PATH
-  export PATH=${ANDROID_SDK_PLATFORM_TOOLS}:$PATH
-  export PATH=${ANDROID_HOME}/emulator:$PATH
+  export PATH=${ANDROID_HOME}/tools:$PATH
+  export PATH=${ANDROID_HOME}/tools/bin:$PATH
+  export PATH=${ANDROID_HOME}/platform-tools:$PATH
 elif [ -d "${SDKPATH_XAMARIN}" ]; then
   export PATH=${SDKPATH_XAMARIN}/platform-tools:$PATH
 fi
