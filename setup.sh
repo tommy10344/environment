dc3c8e94f2a66fbc7825b10011047c7f8388f992
Hiroaki Tomiyoshi <htomiyosi@gmail.com>
2025-03-05 14:12:21 +0900

    Update default Ruby version

--- setup.sh
+++ setup.sh
@@ -97,8 +97,8 @@ brew install ruby
 brew install rbenv
 brew install ruby-build
 eval "$(rbenv init -)"
-rbenv install 3.1.3
-rbenv global 3.1.3
+rbenv install 3.4.1
+rbenv global 3.4.1
 rbenv rehash
 gem install bundler
 gem install iStats
